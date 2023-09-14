7119b3a1f32b92fd601b6f1a105b9680c1d4ad08
Eiks-exe <eiks_art@outlook.com>
2023-09-14 18:49:52 -0400

    fixChannelHubGrid

--- src/app/pages/ChannelsHub/page.tsx
+++ src/app/pages/ChannelsHub/page.tsx
@@ -1,6 +1,7 @@
 import AppChannelCard from '@/app/components/AppChannelCard'
 import React from 'react'
 import AppGrid from '../../components/AppGrid/'
+import { channelsMock } from '@/utils/channelsMock'
 type Props = {}
 
 const Page = (props: Props) => {
@@ -9,7 +10,7 @@ const Page = (props: Props) => {
     ]
   return (
     <div className='mx-auto grid max-w-screen-xxl px-8 py-8 text-center lg:py-16'>
-        <AppGrid data={channels}></AppGrid >
+        <AppGrid data={channelsMock}></AppGrid >
     </div>
   )
 }

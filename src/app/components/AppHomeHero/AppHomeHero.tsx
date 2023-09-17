77ce5fce276f55466453450b8660b455f91b4ef2
Eiks-exe <eiks_art@outlook.com>
2023-09-17 13:11:09 -0400

    backgroundUpdate

--- src/app/components/AppHomeHero/AppHomeHero.tsx
+++ src/app/components/AppHomeHero/AppHomeHero.tsx
@@ -10,7 +10,7 @@ const AppHomeHero = (props: Props) => {
     w={'full'}
     h={'100vh'}
     backgroundImage={
-      'url(https://images.unsplash.com/photo-1600267175161-cfaa711b4a81?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80)'
+      'url(https://images.unsplash.com/photo-1569503169334-e72c72ea5ed1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1964&q=80)'
     }
     backgroundSize={'cover'}
     backgroundPosition={'center center'}>
